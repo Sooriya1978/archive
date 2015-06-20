27f4c8a0f349305073a07fe5e1d0fdde5635f1f9
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2015-06-20 10:05:52 -0300

    questionario 2 -pesquisa villa lobos adicionado (não completo ainda)

---
+++ 2015/pesquisa_biblioteca_villalobos/enunciados_questionario2.R
@@ -0,0 +1,99 @@
+p1 = 'Questionário 2 - 1) Com que frequência você vem ao Parque Villa-Lobos?'
+o1 = c('Diariamente',
+       'Semanalmente',
+       'Quinzenalmente',
+       'Mensalmente',
+       'Raramente',
+       'primeira vez')
+
+p2 = 'Questionário 2 - 2) Você frequenta o Parque Villa-Lobos com qual objetivo?'
+o2 = c('Praticar esportes',
+       'Encontrar amigos',
+       'Passear com familiares',
+       'Ver shows, atividades culturais realizadas no Parque',
+       'Outros')
+
+p3 = 'Questionário 2 - 3) Onde você mora?'
+o3 = c('Grande São Paulo',
+       'SP / Região Norte',
+       'SP / Região Sul',
+       'SP / Região Leste',
+       'SP / Região Oeste',
+       'SP / Centro',
+       'Outras. Onde?')
+
+p4 = 'Questionário 2 - 4) Qual meio de transporte você utiliza para chegar à biblioteca?'
+o4 = c('Ônibus',
+       'Trem',
+       'Carro',
+       'Bicicleta',
+       'Caminhando',
+       'Outros')
+
+p5 = 'Questionário 2 - 5) Você conhece a Biblioteca Villa-Lobos'
+o5 = c('Sim',
+       'Não') 
+
+p6 = 'Questionário 2 - 6) Como ficou sabendo de sua existência?'
+o6 = c('Amigos/ Família', 
+       'Site da Biblioteca', 
+       'Informes, Banners etc.',
+       'Mídia',
+       'Passando pela porta',
+       'Outros') 
+
+
+p7 = 'Questionário 2 - 7) Você frequenta a Biblioteca?'
+o7 = c('Sim',
+       'não')
+
+p8 = 'Questionário 2 - 8) Se sim, com que frequência?'
+o8 = c('Diariamente',
+       'Semanalmente',
+       'Quinzenalmente',
+       'Mensalmente',
+       'Raramente',
+       'primeira vez')
+
+p9 = 'Questionário 2 - 9) Se sim, por quais motivos?'
+o9 = c('Leitura de livros, jornais e revistas',
+       'Emprestimos de livros',
+       'Atividade culturais',
+       'Cursos',
+       'Navegar na internet',
+       'Jogar com os amigos',
+       'Assistir a filmes no PC',
+       'Descançar/Apreciar o ambiente',
+       'Outros')
+
+p10 = 'Questionário 2 - 10) Qual sua escolaridade?'
+o10 = c('Não frequentou a escola',
+        'Ensino Fundamental',
+        'Ensino Médio',
+        'Cursinho pré-vestibular',
+        'Superior',
+        'Pós-graduação')
+
+p11 = 'Questionário 2 - 10) Qual sua idade?'
+o11 = c('de 8 a 15 anos',
+        'de 16 a 20 anos',
+        'de 21 a 25 anos',
+        'de 26 a 30 anos',
+        'de 31 a 35 anos',
+        'de 36 a 40 anos',
+        'de 41 a 45 anos',
+        'de 46 a 50 anos',
+        'de 51 a 60 anos',
+        'mais de 60 anos') 
+
+
+
+questionario1 = list(list(1,p1,o1),
+                     list(2,p2,o2),
+                     list(3,p3,o3),
+                     list(4,p4,o4),
+                     list(5,p5,o5),
+                     list(6,p6,o6),
+                     list(7,p7,o7),
+                     list(8,p8,o8),
+                     list(9,p9,o9))
