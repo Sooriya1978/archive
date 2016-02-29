8638d6d38492470a9a3e46f3127158c8ade792e7
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2016-02-29 11:44:48 -0300

    markdown para melhor vizualização dos resultados

--- 2016/perda_acumulada_salarios_usp/main.R
+++ 2016/perda_acumulada_salarios_usp/main.R
@@ -1,4 +1,4 @@
-setwd('~/Desktop/perda_acumulada_salarios_usp/')
+setwd('~/remota/repos/git.uspdigital.usp.br/archive/2016/perda_acumulada_salarios_usp/')
 
 # 1) Nos arquivos .csv salvei apenas a coluna do piso e so salário corrigido (aproveitei a conta do excel)  
 # 2) Há 3 arquivos: basico.csv, tecnico.csv e superior.csv
@@ -8,23 +8,51 @@ setwd('~/Desktop/perda_acumulada_salarios_usp/')
 
 require(pracma)
 
+# Escrevendo resultados:
+f <- file("output.md","w")
+writeLines("# Estimativa da perda acumulada dos salários USP entre Maio-2011 e Jan-2016 \n",con,sep="\n")
+
+writeLines("\n ## Básico ",con=f,sep="\n")
 basico = read.csv('data/basico.csv')
 x = seq(1:nrow(basico))
 acumulado_piso = trapz(x,basico$piso)
 acumulado_corrigido = trapz(x,basico$corrigido)
-acumulado_corrigido - acumulado_piso
-# R$ 4769,62
+writeLines("Salário PISO acumulado no período: R$ ",con=f,sep="")
+writeLines(gsub('\\.',',',round(acumulado_piso,2)),con=f,sep="\n")
 
+writeLines("Salário CORRIGIDO acumulado no período: R$ ",con=f,sep="")
+writeLines(gsub('\\.',',',round(acumulado_corrigido,2)),con=f,sep="\n")
+perda = acumulado_corrigido - acumulado_piso
+writeLines("Perda acumulada: R$ ",con=f,sep="")
+writeLines(gsub('\\.',',',round(perda,2)),con=f,sep="\n")
+
+writeLines("\n ## Técnico",con=f,sep="\n")
 tecnico = read.csv('data/tecnico.csv')
 x = seq(1:nrow(tecnico))
 acumulado_piso = trapz(x,tecnico$piso)
 acumulado_corrigido = trapz(x,tecnico$corrigido)
-acumulado_corrigido - acumulado_piso
-# R$ 8664,52
+writeLines("Salário PISO acumulado no período: R$ ",con=f,sep="")
+writeLines(gsub('\\.',',',round(acumulado_piso,2)),con=f,sep="\n")
 
+writeLines("Salário CORRIGIDO acumulado no período: R$ ",con=f,sep="")
+writeLines(gsub('\\.',',',round(acumulado_corrigido,2)),con=f,sep="\n")
+perda = acumulado_corrigido - acumulado_piso
+writeLines("Perda acumulada: R$ ",con=f,sep="")
+writeLines(gsub('\\.',',',round(perda,2)),con=f,sep="\n")
+
+writeLines("\n  ## Superior ",con,sep="\n")
 superior = read.csv('data/superior.csv')
 x = seq(1:nrow(superior))
 acumulado_piso = trapz(x,superior$piso)
 acumulado_corrigido = trapz(x,superior$corrigido)
-acumulado_corrigido - acumulado_piso
-# R$ 16292,67
+writeLines("Salário PISO acumulado no período: R$ ",con=f,sep="")
+writeLines(gsub('\\.',',',round(acumulado_piso,2)),con=f,sep="\n")
+
+writeLines("Salário CORRIGIDO acumulado no período: R$ ",con=f,sep="")
+writeLines(gsub('\\.',',',round(acumulado_corrigido,2)),con=f,sep="\n")
+perda = acumulado_corrigido - acumulado_piso
+writeLines("Perda acumulada: R$ ",con=f,sep="")
+writeLines(gsub('\\.',',',round(perda,2)),con=f,sep="\n")
+
+close(con)
+
---
+++ 2016/perda_acumulada_salarios_usp/output.md
@@ -0,0 +1,17 @@
+# Estimativa da perda acumulada dos salários USP entre Maio-2011 e Jan-2016 
+
+
+ ## Básico 
+Salário PISO acumulado no período: R$ 103286,03
+Salário CORRIGIDO acumulado no período: R$ 108055,64
+Perda acumulada: R$ 4769,62
+
+ ## Técnico
+Salário PISO acumulado no período: R$ 187634,83
+Salário CORRIGIDO acumulado no período: R$ 196299,36
+Perda acumulada: R$ 8664,52
+
+  ## Superior 
+Salário PISO acumulado no período: R$ 352826,96
+Salário CORRIGIDO acumulado no período: R$ 369119,64
+Perda acumulada: R$ 16292,67
