8e43cacc1ed2f3158fb00039700c5c4f1160d8f1
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2016-02-28 21:17:25 -0300

    Perda acumulada de salários USP

---
+++ 2016/perda_acumulada_salarios_usp/main.R
@@ -0,0 +1,27 @@
+setwd('~/Desktop/perda_acumulada_salarios_usp/')
+
+# 1) Nos arquivos .csv salvei apenas a coluna do piso e so salário corrigido (aproveitei a conta do excel)  
+# 2) Há 3 arquivos: basico.csv, tecnico.csv e superior.csv
+# 3) Suposição: a inflação de um mês para o outro variou de forma linear, ou seja, 
+# podemos integrar a área sobre de mês em mês e tirar a diferença.
+# O pacote usado para integração pracma.
+
+require(pracma)
+
+basico = read.csv('data/basico.csv')
+x = seq(1:nrow(basico))
+acumulado_piso = trapz(x,basico$piso)
+acumulado_corrigido = trapz(x,basico$corrigido)
+acumulado_corrigido - acumulado_piso
+
+tecnico = read.csv('data/tecnico.csv')
+x = seq(1:nrow(tecnico))
+acumulado_piso = trapz(x,tecnico$piso)
+acumulado_corrigido = trapz(x,tecnico$corrigido)
+acumulado_corrigido - acumulado_piso
+
+superior = read.csv('data/superior.csv')
+x = seq(1:nrow(superior))
+acumulado_piso = trapz(x,superior$piso)
+acumulado_corrigido = trapz(x,superior$corrigido)
+acumulado_corrigido - acumulado_piso
---
+++ 2016/perda_acumulada_salarios_usp/markdown.R
@@ -0,0 +1,2 @@
+setwd('~/Desktop/perda_acumulada_salarios_usp/')
+rmarkdown::render("main.R")
